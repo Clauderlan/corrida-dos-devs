cca68b2deb5ea39e6a76887e2f2ee039c7beba98
Clauderlan <clauderlanbatista@hotmail.com>
2023-05-06 12:53:38 -0300

    Category findById

--- src/main/java/com/c7/corrida/services/CategoryService.java
+++ src/main/java/com/c7/corrida/services/CategoryService.java
@@ -2,6 +2,7 @@ package com.c7.corrida.services;
 
 import com.c7.corrida.entities.Category;
 import com.c7.corrida.repositories.CategoryRepository;
+import com.c7.corrida.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,8 @@ public class CategoryService {
 
     public List<Category> findAll(){
         return categoryRepository.findAll();
+    }
+    public Category findById(Long id){
+        return categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException(id));
     }
 }
